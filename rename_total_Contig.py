0a2c78294205a608b5f3cb28c5956ec0b27d294b
JUNWOOJOK <78505823+JUNWOOJOK@users.noreply.github.com>
2022-05-05 23:26:30 +0900

    Update rename_total_Contig.py

--- rename_total_Contig.py
+++ rename_total_Contig.py
@@ -3,26 +3,29 @@ import sys
 import textwrap
 fasta=sys.argv[1]
 with open(fasta) as file:
-	kk=file.readlines()
+        kk=file.readlines()
 my_dict={}
 length=0
 n=0
 jj=1
 for i in kk:
-	if (i.startswith('>')):
-		n+=1
-		id_1=i.strip()
-		my_dict[id_1]=""
-	if (not i.startswith('>')) and  (jj == n):
-		my_dict[id_1]=my_dict[id_1]+i.strip()
-	if jj != n:
+        if (i.startswith('>')):
+                n+=1
+                id_1=i.strip()
+                my_dict[id_1]=""
+        if (not i.startswith('>')) and  (jj == n):
+                my_dict[id_1]=my_dict[id_1]+i.strip()
+        if jj != n:
                 jj=n
 
 
 
-with open('contig_above_1000','w') as file:
-	for i,l in my_dict.items():
-            if len(l) >= 1000:
+with open('renamed_contig','w') as file:
+        for i,l in my_dict.items():
+            if len(l) >= 0:
                 name=i.split()[0].lstrip('>')
                 i=f'>{fasta}_{name}'
                 file.writelines([i.strip(),'\n',l,'\n'])
+~                                                                                                                                                                                                          
+~                                                                                                                                                                                                          
+~               
